c54c1859be7d4dd438b36a76050904e515b69962
gaelmiau <gaelmartinez050@gmail.com>
2025-09-22 19:00:56 -0600

    cambio de tamaño a Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,13 @@
-
 export function Header() {
   return (
-    <header className="py-6 px-4 md:px-8 border-b-2 border-primary/20 bg-white">
+    <header className="py-3 px-4 md:px-8 border-b-2 border-primary/20 bg-white">
       <div className="container mx-auto flex items-center justify-center gap-4">
-        {/* Imagen a la izquierda */}
-        <img src="/icono.png" alt="Logo" className="w-12 h-12 md:w-16 md:h-16"></img>
-
-        {/* Título */}
-        <h1 className="text-3xl md:text-5xl font-bold text-center tracking-tight text-blue-800">
+        <img src="/icono.png" alt="Logo" className="w-10 h-10 md:w-12 md:h-12" />
+        <h1 className="text-2xl md:text-4xl font-bold text-center tracking-tight text-blue-800">
           Lotería Seguridad de la Información
         </h1>
       </div>
     </header>
-
-
   );
 }
 
